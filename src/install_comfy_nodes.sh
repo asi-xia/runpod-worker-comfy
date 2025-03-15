1bbbc41e21685f1f2046da7c3aa94ee0e128ad53
asi-xia <130675778+asi-xia@users.noreply.github.com>
2025-03-15 17:24:14 +0800

    Update install_comfy_nodes.sh

--- src/install_comfy_nodes.sh
+++ src/install_comfy_nodes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for node_item in comfyui_controlnet_aux comfyui_ipadapter_plus comfyui-cogvideoxwrapper comfyui-custom-scripts comfyui-hunyuanvideowrapper comfyui-impact-pack comfyui-kolors-mz comfyui-mixlab-nodes comfyui-videohelpersuite easyanimate rgthree-comfy was-node-suite-comfyui
+for node_item in comfyui_controlnet_aux comfyui_ipadapter_plus comfyui-cogvideoxwrapper comfyui-custom-scripts comfyui-hunyuanvideowrapper comfyui-impact-pack comfyui-kolors-mz comfyui-mixlab-nodes comfyui-videohelpersuite easyanimate rgthree-comfy
     do
         echo "install $node_item"
         comfy node registry-install $node_item
