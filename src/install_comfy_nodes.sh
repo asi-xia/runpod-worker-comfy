5c4307ac73611f407259e76aaaa8543508fb6911
asi-xia <130675778+asi-xia@users.noreply.github.com>
2025-03-15 16:47:28 +0800

    Update install_comfy_nodes.sh

--- src/install_comfy_nodes.sh
+++ src/install_comfy_nodes.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for node_item in comfyui_controlnet_aux comfyui_ipadapter_plus comfyui-cogvideoxwrapper comfyui-custom-scripts comfyui-hunyuanvideowrapper comfyui-impact-pack comfyui-kolors-mz comfyui-mixlab-nodes comfyui-videohelpersuite easyanimate rgthree-comfy was-node-suite-comfyui
     do
