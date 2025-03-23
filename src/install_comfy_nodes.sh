da3caa2b3a1d3ce0d5897f09100cbbac83ebdcde
asixia <130675778+asi-xia@users.noreply.github.com>
2025-03-23 19:47:12 +0800

    modified:   Dockerfile modified:   src/install_comfy_nodes.sh

--- src/install_comfy_nodes.sh
+++ src/install_comfy_nodes.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 for node_item in comfyui_controlnet_aux comfyui_ipadapter_plus comfyui-cogvideoxwrapper comfyui-custom-scripts comfyui-hunyuanvideowrapper comfyui-impact-pack comfyui-kolors-mz comfyui-mixlab-nodes comfyui-videohelpersuite easyanimate rgthree-comfy
     do
         echo "install $node_item"
